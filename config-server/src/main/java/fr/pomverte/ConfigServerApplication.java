8abe366c9f69bfe75198f798fe587d2d2c8bc911
pomverte <vietnem@yahoo.fr>
2017-01-12 01:11:18 +0100

    push configuration via RabbitMQ

--- config-server/src/main/java/fr/pomverte/ConfigServerApplication.java
+++ config-server/src/main/java/fr/pomverte/ConfigServerApplication.java
@@ -4,11 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+// tag::asciidoc-EnableConfigServer[]
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
+// end::asciidoc-EnableConfigServer[]
 
-	public static void main(String[] args) {
-		SpringApplication.run(ConfigServerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(ConfigServerApplication.class, args);
+    }
 }
